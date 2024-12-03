68b90845a95129f6386c51d036210a4c10c6d863
Rauder07 <jeremy.jesus.tauro07@gmail.com>
2024-12-03 16:57:43 -0500

    Ready to deploy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
-module.exports = async function(eleventyConfig) {
+module.exports =  function(eleventyConfig) {
 	// Configure Eleventy
+
+    eleventyConfig.addPassthroughCopy("code/css");
+    eleventyConfig.addPassthroughCopy("code/js");
+    eleventyConfig.addPassthroughCopy("code/img");
+
     return{
         dir:{
             input:"code",
