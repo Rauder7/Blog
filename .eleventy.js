d4c788997396784c39b678b8cd9145a0aa6f34f2
Rauder07 <jeremy.jesus.tauro07@gmail.com>
2024-12-03 16:57:43 -0500

    Started Project ready to deploy in github-pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
+
+
 module.exports = async function(eleventyConfig) {
 	// Configure Eleventy
+
+    eleventyConfig.addPassthroughCopy("code/css");
+    eleventyConfig.addPassthroughCopy("code/js");
+    eleventyConfig.addPassthroughCopy("code/img");
+    
+    const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
+
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
     return{
         dir:{
             input:"code",
